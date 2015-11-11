cf356ac3c9abb015e063e799ee63b3cc2dd47b82
暮晨 <362864550@qq.com>
2015-11-11 18:21:25 +0800

    增加对bing的调用

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 #-*- coding=utf-8 -*-
 
 from SearchEngine import baidu
+from SearchEngine import bing
 
 
 if __name__=="__main__":
     bd = baidu.Baidu_Search()
     bd.titleCompare(bd.pageGet())
-
+    bi = bing.Bing_Search()
+    urls = bi.pageGet()
+    titles=bi.titleGet(urls)
+    bi.titleCompare(titles)
 
 
 
