dc51b5a57c1ad69f7d2c1dd52ad9dd7d5632d779
暮晨 <362864550@qq.com>
2015-11-13 13:03:29 +0800

    增加一个requests函数

--- SearchEngine/Common/network.py
+++ SearchEngine/Common/network.py
@@ -7,6 +7,85 @@ import requests
 from bs4 import BeautifulSoup
 from . import function as fun
 
+
+#页面读取函数
+def Request(url,header):
+    '''
+    获取网页后不处理
+    返回一个requests类型的连接
+    出错返回一个None
+    '''
+    print 'dataRequest'
+    flag = fun.sslJudge(url)
+    count = 0
+    while True:
+        try:
+            page = requests.get( url , headers = header, timeout = 10 , verify = flag )
+        except requests.exceptions.ConnectionError:
+            print 'ConnectionError'
+            if flag == True:
+                flag = False
+                count += 1
+                continue
+            if count > 1:
+                return None
+            else:
+                count += 1
+                continue
+        except requests.exceptions.Timeout:#this is important
+            print 'Timeout'
+            return None
+        except requests.exceptions.ConnectTimeout:
+            print 'ConnectTimeout'
+            if count > 1:
+                return None
+            else:
+                count += 1
+                continue
+        except requests.exceptions.ReadTimeout:
+            print 'ReadTimeout'
+            if count > 1:
+                return None
+            else:
+                count += 1
+                continue
+        except requests.exceptions.TooManyRedirects:
+            print 'TooManyRedirects'
+            return None
+        except requests.exceptions.SSLError:
+            print 'SSLError'
+            flag = False
+            continue
+        except requests.exceptions.HTTPError:
+            print 'HTTPError'
+            return None
+        except requests.exceptions.RequestException as e:
+            errortext = "Error in function : \" %s \" ,\n \
+                Error name is : \" %s \" ,\n \
+                Error type is : \" %s \" ,\n \
+                Error Message is : \" %s \" ,\n \
+                Error doc is : \" %s \" \n" % \
+            (sys._getframe().f_code.co_name,\
+                 e.__class__.__name__,\
+                 e.__class__,\
+                 e,\
+                 e.__class__.__doc__)
+            print errortext
+            return None
+        else:
+            if page.status_code == 200:
+                return page
+                break
+            else:
+                errortext = "Page Code %s " % page.status_code
+                print errortext
+                if count > 1:
+                    return None
+                else:
+                    count += 1
+                    continue
+
+
 #页面读取函数
 def dataRequest(url,header):
     '''
